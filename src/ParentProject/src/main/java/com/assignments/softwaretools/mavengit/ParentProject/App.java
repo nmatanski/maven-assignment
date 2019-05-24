1c88614731b5527f9effce1934f2eb75159b8a15
nmatanski <nmatanski95@abv.bg>
2019-05-24 17:43:32 +0300

    Initial commit

---
+++ src/ParentProject/src/main/java/com/assignments/softwaretools/mavengit/ParentProject/App.java
@@ -0,0 +1,13 @@
+package com.assignments.softwaretools.mavengit.ParentProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
